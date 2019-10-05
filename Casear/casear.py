4440febc0dce49084b2f0226519dcbeee1b9a1ad
Torak28 <zelechowski28@gmail.com>
2019-10-05 18:02:39 +0200

    New version with cli on board :clap:

--- Casear/casear.py
+++ Casear/casear.py
@@ -1,28 +1,56 @@
+import sys, getopt
 
-msg = 'Hello World'
-key = 1
-mode = 'encrypt'
 
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkllmnopqrstuvwxyz1234567890 !?.,'
+def main():
+    try:
+        msg, key, mode = None, None, None
 
-translated = ''
+        opts, args = getopt.getopt(sys.argv[1:], "hedm:k:", ['help', 'encrypt', 'decrypt', 'msg=', 'key='])
+        if len(args) > 0 or len(opts) != 3:
+            print('Argument error')
+            sys.exit(2)
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                print('casear.py\n\t-e to encrypt\n\t-d to decrypt\n\t-m to pass message\n\t-k to pass key')
+            elif o in ('-m', '--msg'):
+                msg = a
+            elif o in ('-k', '--key'):
+                key = int(a)
+            elif o in ('-e', '--encrypt'):
+                mode = 'encrypt'
+            elif o in ('-d', '--decrypt'):
+                mode = 'decrypt'
+        if msg is not None and key is not None and mode is not None:
+            casear(msg, key, mode)
+        else:
+            print(f'Argument error\n\tmsg: {msg},\n\tkey: {key},\n\tmode: {mode}')
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
 
-for symbol in msg:
-    if symbol in SYMBOLS:
-        symbolIndex = SYMBOLS.find(symbol)
+def casear(msg, key, mode):
+    SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkllmnopqrstuvwxyz1234567890 !?.,'
+    translated = ''
 
-        if mode == 'encrypt':
-            translatedIndex = symbolIndex + key
-        elif mode == 'decrypt':
-            translatedIndex = symbolIndex - key
+    for symbol in msg:
+        if symbol in SYMBOLS:
+            symbolIndex = SYMBOLS.find(symbol)
 
-        if translatedIndex >= len(SYMBOLS):
-            translatedIndex = translatedIndex - len(SYMBOLS)
-        elif translatedIndex < 0:
-            translatedIndex = translatedIndex + len(SYMBOLS)
+            if mode == 'encrypt':
+                translatedIndex = symbolIndex + key
+            elif mode == 'decrypt':
+                translatedIndex = symbolIndex - key
 
-        translated = translated + SYMBOLS[translatedIndex]
-    else:
-        translated = translated + symbol
+            if translatedIndex >= len(SYMBOLS):
+                translatedIndex = translatedIndex - len(SYMBOLS)
+            elif translatedIndex < 0:
+                translatedIndex = translatedIndex + len(SYMBOLS)
 
-print(translated)
+            translated = translated + SYMBOLS[translatedIndex]
+        else:
+            translated = translated + symbol
+
+    print(translated)
+
+if __name__ == '__main__':
+    main()
