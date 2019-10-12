6f656ce1df3f7351c6a1d417227733af80ffcf9b
Torak28 <zelechowski28@gmail.com>
2019-10-12 15:29:08 +0200

    Test has some cli func here and there

--- Transposition/test.py
+++ Transposition/test.py
@@ -2,18 +2,39 @@ import random
 import sys
 import trans
 import decryptTrans
+import argparse
 
 def main():
-    random.seed(42)
+    seed, length, how_many = None, None, None
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--seed', help='Just a random seed number', default=42, type=int)
+    parser.add_argument('-l', '--length', help='Length of random msg to check', default=40, type=int)
+    parser.add_argument('-hm', '--how_many', help='How many test should be run', default=20, type=int)
 
-    for i in range(20):
-        msg = 'ABCDEFGHIJKLMNOPQRSTUWXYZ' * random.randint(4, 40)
+    args = parser.parse_args()
+
+    seed = args.seed
+    length = args.length
+    how_many = args.how_many
+
+    if seed == None or length == None or how_many == None:
+        print('Argument error. See --help')
+        sys.exit(2)
+
+    test(seed, length, how_many)
+
+
+def test(s, l, h_m):
+    random.seed(s)
+
+    for i in range(h_m):
+        msg = 'ABCDEFGHIJKLMNOPQRSTUWXYZ' * random.randint(4, l)
 
         msg = list(msg)
         random.shuffle(msg)
         msg = ''.join(msg)
 
-        print(f'Test #{str(i)}: {str(msg[:20])} |')
+        print(f'Test #{str(i + 1)}: {str(msg[:50])}...')
 
         for key in range(1, int(len(msg)/2)):
             enc = trans.trans(msg, key)
